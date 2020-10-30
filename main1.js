727570fa1af7b088946e3ba405ae008ca16cb6d5
Clarence Kyle Perez Menor <71060948+KyleEL@users.noreply.github.com>
2020-10-30 05:20:01 -0700

    Add files via upload

--- main1.js
+++ main1.js
@@ -1,14 +1,55 @@
-const questions = document.querySelectorAll("qna");
+$(document).ready(function() {
+    $(".question").click(function() {
+        //Test 1 - Working
+        /* alert("Almost There"); */
+        //Test 2 - Working
+        /* $(".icon").css("border","1px solid black"); */
+        $(this).toggleClass("bold");
+        $(this, "img").toggleClass("rotate"); //Working but not the intended way
+
+    });
+
+    $(".icon").click(function() {
+        //Test 1 - Working
+        /* alert("Almost"); */
+        //Test 2 - Working
+        /* $(".question").css("border","1px solid black"); */
+        $(this).toggleClass("rotate"); //Working
+    });
+});
+
+/* document.querySelectorAll("qContainer").addEventListener("click", function() {
+    document
+}); */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* const questions = document.querySelectorAll("qna");
 
 questions.forEach((q) => {
-    const icon = q.querySelector(".icon");
+    const icon = q.querySelector("icon");
     const title = q.querySelector("question");
 
     icon.classList.toggle("rotate");
 
     //Event Listener for the questions.
     title.addEventListener("click", () => {
-        /* showAnswer(); */ 
+        showAnswer(); 
         
         //For Trial Purposes - Even this won't work
         alert("hey");
@@ -28,4 +69,4 @@ questions.forEach((q) => {
         quest.classList.toggle("visible");
     }
 
-});
+}); */
